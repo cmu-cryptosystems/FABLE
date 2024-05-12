f97d085c8bb58907c84282664e2521da65350fb3
Zhengyuan <su-zy21@mails.tsinghua.edu.cn>
2024-05-12 13:09:08 +0800

    Update BatchPIR: hash over OPRF.

--- src/batchlut.cpp
+++ src/batchlut.cpp
@@ -69,26 +69,22 @@ void bench_lut() {
 	
     osuCrypto::PRNG prng(osuCrypto::sysRandomSeed());
 
-    vector<keyblock> lowmc_keys(w); 
-    vector<prefixblock> lowmc_prefixes(w); 
-    vector<oc::block> aes_keys(w);
-    vector<std::bitset<128-DatabaseConstants::InputLength>> aes_prefixes(w);
+    keyblock lowmc_key; 
+    prefixblock lowmc_prefix; 
+    oc::block aes_key;
+    std::bitset<128-DatabaseConstants::InputLength> aes_prefix;
 	if (party == ALICE) {
 		if (hash_type == HashType::LowMC) {
-			for (size_t i = 0; i < NumHashFunctions; i++) {
-				lowmc_keys[i] = random_bitset<utils::keysize>(&prng);
-				lowmc_prefixes[i] = random_bitset<utils::prefixsize>(&prng);
-			}
+			lowmc_key = random_bitset<utils::keysize>(&prng);
+			lowmc_prefix = 0; // random_bitset<utils::prefixsize>(&prng);
 		} else {
-			for (size_t i = 0; i < NumHashFunctions; i++) {
-				aes_keys[i] = prng.get<oc::block>();
-				aes_prefixes[i] = random_bitset<128-DatabaseConstants::InputLength>(&prng);
-			}
-			auto message_string = concatenate(aes_prefixes[0], rawinputblock(plain_queries[0])).to_string();
+			aes_key = prng.get<oc::block>();
+			aes_prefix = random_bitset<128-DatabaseConstants::InputLength>(&prng);
+			auto message_string = concatenate(aes_prefix, rawinputblock(plain_queries[0])).to_string();
 			uint64_t high_half = std::bitset<64>(message_string.substr(0, 64)).to_ullong();
 			uint64_t low_half = std::bitset<64>(message_string.substr(64)).to_ullong();
 			oc::block message(high_half, low_half);
-			auto c = oc::AES(aes_keys[0]).ecbEncBlock(message).get<uint64_t>();
+			auto c = oc::AES(aes_key).ecbEncBlock(message).get<uint64_t>();
 		}
 	}
 
@@ -99,68 +95,59 @@ void bench_lut() {
 
 	// prepare batch
 	start_record(io_gc, "Batch Preparation");
-    vector<vector<string>> batch(batch_size, vector<string>(w));
-	vector<sci::LowMC> lowmc_ciphers_2PC;
-	vector<sci::AES> aes_ciphers_2PC;
+    vector<string> batch(batch_size);
+	sci::LowMC* lowmc_ciphers_2PC;
+	sci::AES* aes_ciphers_2PC;
 
 	if (hash_type == HashType::LowMC) {
-		for (int hash_idx = 0; hash_idx < w; hash_idx++) {
-			lowmc_ciphers_2PC.emplace_back(sci::LowMC(lowmc_keys[hash_idx], ALICE, batch_size));
+		lowmc_ciphers_2PC = new sci::LowMC(lowmc_key, ALICE, batch_size);
+
+		secret_block m;
+		// Integer secret_prefix = share_bitset(lowmc_prefix, ALICE);
+		for (int j = 0; j < sci::blocksize; j++) {
+			m[j].bits.resize(batch_size);
+			if (j >= bitlength+1) {
+				m[j].bits.assign(batch_size, Bit(0));
+				// m[j].bits.assign(batch_size, secret_prefix[j-bitlength-1]);
+			} else {
+				for (int i = 0; i < batch_size; i++) {
+					m[j][i] = secret_queries[i][j];
+				}
+			}
 		}
 
-		vector<secret_block> m(w);
-		for (int hash_idx = 0; hash_idx < w; hash_idx++) {	
-			Integer secret_prefix = share_bitset(lowmc_prefixes[hash_idx], ALICE);
+		secret_block c;
+		c = lowmc_ciphers_2PC->encrypt(m); // blocksize, batchsize
+		for (int i = 0; i < batch_size; i++) {
+			block hash_out;
 			for (int j = 0; j < sci::blocksize; j++) {
-				m[hash_idx][j].bits.resize(batch_size);
-				if (j >= bitlength+1) {
-					m[hash_idx][j].bits.assign(batch_size, secret_prefix[j-bitlength-1]);
-				} else {
-					for (int i = 0; i < batch_size; i++) {
-						m[hash_idx][j][i] = secret_queries[i][j];
-					}
-				}
+				hash_out[j] = c[j][i].reveal(BOB);
 			}
-		}
-
-		vector<secret_block> c(w);
-		for (int hash_idx = 0; hash_idx < w; hash_idx++) {
-			c[hash_idx] = lowmc_ciphers_2PC[hash_idx].encrypt(m[hash_idx]); // blocksize, batchsize
-		}
-		for (int hash_idx = 0; hash_idx < w; hash_idx++) {
-			for (int i = 0; i < batch_size; i++) {
-				block hash_out;
-				for (int j = 0; j < sci::blocksize; j++) {
-					hash_out[j] = c[hash_idx][j][i].reveal(BOB);
-				}
-				batch[i][hash_idx] = hash_out.to_string();
-			}
+			batch[i] = hash_out.to_string();
 		}
 	} else {
-		vector<vector<Integer>> m(w, vector<Integer>(batch_size));
-		vector<vector<Integer>> c(w);
-		for (int hash_idx = 0; hash_idx < w; hash_idx++) {
-			Integer key(128, 0);
-			auto data = aes_keys[hash_idx].get<uint64_t>();
-			auto key_bitset = concatenate(std::bitset<64>(data[1]), std::bitset<64>(data[0]));
-			for (int i = 0; i < 128; i++) {
-				key[i] = Bit(key_bitset[i], ALICE);
-			}
-			aes_ciphers_2PC.emplace_back(sci::AES(key));
+		vector<Integer> m(batch_size);
+		vector<Integer> c;
+		Integer key(128, 0);
+		auto data = aes_key.get<uint64_t>();
+		auto key_bitset = concatenate(std::bitset<64>(data[1]), std::bitset<64>(data[0]));
+		for (int i = 0; i < 128; i++) {
+			key[i] = Bit(key_bitset[i], ALICE);
 		}
+		aes_ciphers_2PC = new sci::AES(key);
 		for (int hash_idx = 0; hash_idx < w; hash_idx++) {	
-			Integer secret_prefix = share_bitset(aes_prefixes[hash_idx], ALICE);
+			Integer secret_prefix = share_bitset(aes_prefix, ALICE);
 			for (int j = 0; j < batch_size; j++) {
-				m[hash_idx][j] = secret_queries[j];
-				m[hash_idx][j].bits.insert(m[hash_idx][j].bits.end(), secret_prefix.bits.begin(), secret_prefix.bits.end());
+				m[j] = secret_queries[j];
+				m[j].bits.insert(m[j].bits.end(), secret_prefix.bits.begin(), secret_prefix.bits.end());
 			}
-			c[hash_idx] = aes_ciphers_2PC[hash_idx].EncryptECB(m[hash_idx]);
+			c = aes_ciphers_2PC->EncryptECB(m);
 			for (int i = 0; i < batch_size; i++) {
 				std::bitset<128> hash_out;
 				for (int j = 0; j < 128; j++) {
-					hash_out[j] = c[hash_idx][i][j].reveal(BOB);
+					hash_out[j] = c[i][j].reveal(BOB);
 				}
-				batch[i][hash_idx] = hash_out.to_string();
+				batch[i] = hash_out.to_string();
 			}
 		}
 	}
@@ -266,9 +253,9 @@ void bench_lut() {
 		batch_server->populate_raw_db(generator);
 		
 		if (params.get_hash_type() == HashType::LowMC) {
-			batch_server->lowmc_prepare(lowmc_keys, lowmc_prefixes);
+			batch_server->lowmc_prepare(lowmc_key, lowmc_prefix);
 		} else {
-			batch_server->aes_prepare(aes_keys, aes_prefixes);
+			batch_server->aes_prepare(aes_key, aes_prefix);
 		}
 		batch_server->initialize();
 
