0a26c98f049487fe48bf2e6e1d1515adfade91ac
Zhengyuan <su-zy21@mails.tsinghua.edu.cn>
2025-06-02 14:55:32 +0000

    Add const reference to LUT.

--- src/OT/splut.h
+++ src/OT/splut.h
@@ -3,5 +3,5 @@
 #include <cstdint>
 #include "coproto/Socket/AsioSocket.h"
 
-std::vector<uint32_t> SPLUT(std::vector<uint64_t> &T, std::vector<uint32_t> x, uint64_t l_out, uint64_t l_in, int party, coproto::AsioSocket& chl, uint64_t numThreads = 1);
+std::vector<uint32_t> SPLUT(const std::vector<uint64_t> &T, std::vector<uint32_t> x, uint64_t l_out, uint64_t l_in, int party, coproto::AsioSocket& chl, uint64_t numThreads = 1);
 std::vector<uint32_t> SPLUT(std::map<uint64_t, uint64_t> &T, std::vector<uint32_t> x, uint64_t l_out, uint64_t l_in, int party, coproto::AsioSocket& chl, uint64_t numThreads = 1);
